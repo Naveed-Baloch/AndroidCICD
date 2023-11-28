462b45a47f6a8d2c7cb70cc3789a36798c99cc30
Naveed-Baloch <m.naveed32001@gmail.com>
2023-11-28 11:03:13 +0500

    add ci-cd file

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,13 @@ plugins {
 
 android {
     namespace = "com.naveed.androidcicd"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.naveed.androidcicd"
         minSdk = 24
-        targetSdk = 33
+        //noinspection EditedTargetSdkVersion
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
